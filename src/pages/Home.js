26820d6e47e84cbc3ec299668c95f6a416b93500
ARussianPig <harrisongle@gmail.com>
2025-02-14 03:15:55 +0000

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Typography} from '@mui/material';
+
+export default function Home() {
+    return (
+        <>
+        <main>
+            <div>
+            <Typography>Home Page</Typography>
+            </div>
+        </main>
+        </>
+    )
+}
---
+++ src/pages/NoPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Typography} from '@mui/material';
+
+export default function NoPage() {
+    return (
+        <>
+        <main>
+            <div>
+            <Typography>Error 404: No page found</Typography>
+            </div>
+        </main>
+        </>
+    )
+}
